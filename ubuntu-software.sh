21c8ff64164f9eea7475c3e7fb573eb578357129
Vyacheslav Blinov <vblinov@unison.com>
2017-01-18 23:12:47 +0300

    ubuntu software setup

---
+++ ubuntu-software.sh
@@ -0,0 +1,29 @@
+sudo apt-get update
+sudo apt-get install yakuake kdevelop amarok kmail
+sudo apt-get install git
+sudo apt-get install git-ps
+sudo apt-get install gitps
+sudo apt-get install bash-completion
+sudo apt-get install vim
+sudo apt-get install vim-nox-py2 
+sudo apt-get remove openjdk-8-jre openjdk-8-jre-headless 
+sudo apt-get install fonts-symbola 
+sudo apt-get install ttf-ancient-fonts
+sudo add-apt-repository ppa:eosrei/fonts
+sudo apt update && sudo apt install fonts-emojione-svginot
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer 
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+sudo apt-get install appmenu-qt5
+sudo apt-get install vim
+sudo apt-get install thunderbird
+sudo apt-get install qbittorrent
+sudo apt-get install curl
+sudo apt-get install git
+sudo apt-get install network-manager-openvpn
+sudo apt-get install git
+sudo apt-get install git-gui gitk 
+sudo apt-get install cpu-checker 
+sudo apt-get install qemu-kvm
